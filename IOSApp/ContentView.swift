991a229785863e1e3bb577d6aecb77b2e97bc26e
Student3 <student3@NIBMADMIN06s-iMac.local>
2026-06-06 16:37:09 +0530

    init

---
+++ IOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSApp
+//
+//  Created by Student3 on 2026-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, from world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSApp/IOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppApp.swift
+//  IOSApp
+//
+//  Created by Student3 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
